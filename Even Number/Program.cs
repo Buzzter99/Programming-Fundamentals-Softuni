d7930f2e5da86a457e41e1f9a4f72c9d8fc2ea91
Svetoslav Busarov <103368248+Buzzter99@users.noreply.github.com>
2022-09-20 19:35:18 +0300

    Add files via upload

---
+++ Even Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Even_Number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            var input = int.Parse(Console.ReadLine());
+            //int result = input;
+
+            while (input % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+
+
+                input = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The number is: {Math.Abs(input)}");
+        }
+    }
+}
