2b894299378044dd1616879987d9fb5219007170
Svetoslav Busarov <103368248+Buzzter99@users.noreply.github.com>
2022-09-20 19:34:44 +0300

    Add files via upload

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var result = secondNumber;
+            if (secondNumber > 10)
+                Console.WriteLine($"{number} X {secondNumber} = {number * secondNumber}");
+            else
+                for (var i = 1; i <= 10; i++)
+                {
+                    if (result > 10) break;
+                    var res = number * i;
+                    Console.WriteLine($"{number} X {result} = {number * result}");
+                    result++;
+                }
+        }
+    }
+}
